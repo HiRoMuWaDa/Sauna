1d73a0c625347d994e2cf5158a08afdd045ca80b
honda <kinnjiroa@gmail.com>
2022-12-06 08:46:19 +0900

    更新

--- src/test/java/com/example/ecommerce_a/controller/OrderControllerTest.java
+++ src/test/java/com/example/ecommerce_a/controller/OrderControllerTest.java
@@ -32,6 +32,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.example.ecommerce_a.domain.Item;
 import com.example.ecommerce_a.domain.Order;
 import com.example.ecommerce_a.domain.OrderItem;
+import com.example.ecommerce_a.domain.User;
 import com.example.ecommerce_a.form.OrderConfirmForm;
 import com.example.ecommerce_a.util.CsvDataSetLoader;
 import com.example.ecommerce_a.util.SessionUtil;
@@ -87,7 +88,6 @@ class OrderControllerTest {
 		}
 	}
 //		assertEquals(3, orderItemList.size(), "注文確認画面商品件数エラー");
-	// orderItemの取り出し方（for文？）
 
 	@DatabaseSetup("/order_confirm")
 	@DisplayName("注文確認画面(ポイントリスト)")
@@ -104,46 +104,29 @@ class OrderControllerTest {
 		List<Integer> usablePointlist = (List<Integer>) mav.getModel().get("usablePointList");
 
 	}
-
-	@DatabaseSetup("/user_point")
-	@ExpectedDatabase(value = "/user_pointEx", assertionMode = DatabaseAssertionMode.NON_STRICT)
-	@DisplayName("注文確認画面(ポイント使用")
 	@Test
-	void UsePoint() throws Exception {
-		MockHttpSession session = SessionUtil.createShoppingCartIdAdItemSession();// ログイン処理
-		MvcResult mvcResult = mockMvc.perform(get("/shop/order-confirmed").session(session))
-				.andExpect(view().name("order_confirm_pointUsable"))// 遷移先のHTML
-				.andReturn();// 元のURL
-
-		// usablePointListの確認（リクエストｽｺｰﾌﾟからの取り出し）
-
-	}
-
-	@DatabaseSetup("/order_confirm")
-	@DisplayName("注文確認画面(本日注文で3時間以内の場合はエラー)")
-	@Test
-	void testPoint() throws Exception {
-
-		MockHttpSession session = SessionUtil.createShoppingCartIdAdItemSession();// ログイン処理
-		MvcResult mvcResult = mockMvc.perform(get("/shop/order-confirmed").session(session))
-				.andExpect(view().name("order_confirm_pointUsable"))// 遷移先のHTML
-				.andReturn();// 元のURL
-		ModelAndView mav = mvcResult.getModelAndView();// modelの使用
-		List<Integer> usablePointlist = (List<Integer>) mav.getModel().get("現在より3時間後以降の日時をご入力ください");
-
-		// usablePointListの確認（リクエストｽｺｰﾌﾟからの取り出し）
-
-	}
-
-//	}
-	// useblePointListから一つ取り出すのか
-	// どうやってuseblePointの期待値を作るのか
-	// ↑リストをセッションで作成してそこの値を取り出す
-
+	   
+		@ExpectedDatabase(value = "/use_pointEx", assertionMode = DatabaseAssertionMode.NON_STRICT)
+		@DisplayName("ポイント使用")
+		void PointUsed() throws Exception {
+			MockHttpSession session = SessionUtil.createOrder();
+		
+			MvcResult mvcResult = mockMvc.perform(get("/shop/order-confirmed")// パラメータの処理
+		
+					.session(session).param("destinationName", "テストユーザ")
+					.param("destinationEmail", "coffeeshop.test@gmail.com").param("destinationAddress", "テスト住所")
+					.param("destinationTel", "000-0000-0000").param("destinationZipcode", "111-1111")
+					.param("paymentMethod", "1").param("usePoint", "100").param("deliveryDate", "2022-12-10")
+					.param("deliveryTime", "14:00"))
+					.andExpect(view().name("redirect:/shop/order-finished"))// 遷移先のHTML
+					.andReturn();
+			User use= (User) session.getAttribute("use");
+		}
+	
 	@Test
    @DatabaseSetup("/order_finished")
 	@ExpectedDatabase(value = "/order_finishedEx", assertionMode = DatabaseAssertionMode.NON_STRICT)
-	@DisplayName("オーダー内容")
+	@DisplayName("オーダー内容(代引き)")
 	void testOrderCompletion01() throws Exception {
 		MockHttpSession session = SessionUtil.createOrder();
 	
@@ -152,24 +135,54 @@ class OrderControllerTest {
 				.session(session).param("destinationName", "テストユーザ")
 				.param("destinationEmail", "coffeeshop.test@gmail.com").param("destinationAddress", "テスト住所")
 				.param("destinationTel", "000-0000-0000").param("destinationZipcode", "111-1111")
-				.param("paymentMethod", "1").param("usePoint", "0").param("deliveryDate", "2022-12-20")
-				.param("deliveryTime", "14:00")).andExpect(view().name("redirect:/shop/order-finished")
-						)// 遷移先のHTML
+				.param("paymentMethod", "1").param("usePoint", "100").param("deliveryDate", "2022-12-10")
+				.param("deliveryTime", "14:00"))
+				.andExpect(view().name("redirect:/shop/order-finished"))// 遷移先のHTML
 				.andReturn();
-
-		ModelAndView mav = mvcResult.getModelAndView();// modelの使用
-            Order order =(Order) mav.getModel().get("order");
-		
-			assertEquals(1, order.getId(), "idが一致していない");
-		
-
-	
-
+		User use= (User) session.getAttribute("use");
 	}
-	@DisplayName("注文履歴確認画面(件数あり)")
+	@Test
+	   @DatabaseSetup("/order_finished")
+		@ExpectedDatabase(value = "/order_finishedEx", assertionMode = DatabaseAssertionMode.NON_STRICT)
+		@DisplayName("オーダー内容(クレジット)")
+		void testOrderCompletion02() throws Exception {
+			MockHttpSession session = SessionUtil.createOrder();
+		
+			MvcResult mvcResult = mockMvc.perform(get("/shop/order-confirmed")// パラメータの処理
+		
+					.session(session).param("destinationName", "テストユーザ")
+					.param("destinationEmail", "coffeeshop.test@gmail.com").param("destinationAddress", "テスト住所")
+					.param("destinationTel", "000-0000-0000").param("destinationZipcode", "111-1111")
+					.param("paymentMethod", "2").param("usePoint", "100").param("deliveryDate", "2022-12-10")
+					.param("deliveryTime", "14:00"))
+					.andExpect(view().name("redirect:/shop/order-finished"))// 遷移先のHTML
+					.andReturn();
+			User use= (User) session.getAttribute("use");
+	}
+	@DisplayName("注文エラー")
 
 	@Test
 
+	@DatabaseSetup("/order_error")
+	@ExpectedDatabase(value = "/order_finishedEx", assertionMode = DatabaseAssertionMode.NON_STRICT)
+
+	void OrderErrorTest() throws Exception {
+		MockHttpSession session = SessionUtil.createOrder();
+		
+		MvcResult mvcResult = mockMvc.perform(get("/shop/order-confirmed")// パラメータの処理
+	
+				.session(session).param("destinationName", "テストユーザ")
+				.param("destinationEmail", "coffeeshop.test@gmail.com").param("destinationAddress", "テスト住所")
+				.param("destinationTel", "000-0000-0000").param("destinationZipcode", "111-1111")
+				.param("paymentMethod", "1").param("usePoint", "100").param("deliveryDate", "2022-12-01")
+				.param("deliveryTime", "14:00"))
+				.andExpect(view().name("redirect:/shop/order-finished"))// 遷移先のHTML
+				.andReturn();
+		ModelAndView mav = mvcResult.getModelAndView();
+		mav.getModel().get("deliveryTimeError");
+	}
+	@DisplayName("注文履歴確認画面(件数あり)")
+	@Test
 	@DatabaseSetup("/test_order")
 
 	void OrderHisttoryTest() throws Exception {
@@ -189,19 +202,10 @@ class OrderControllerTest {
 			OrderItem orderItem = orderItemList.get(0);
 
 			assertEquals(1, orderItem.getItemId(), "idが一致していない");
-
-		}
-		// エラー内容→charの文字数を超える
-		// DB,VSコードの文字化け確認
-
-	}
-
+			}
+}
 	@DisplayName("注文履歴確認画面(件数無し)")
-
 	@Test
-
-//				    @DatabaseSetup("/null_order")
-
 	void OrderHisttoryTest_A() throws Exception {
 		MockHttpSession session = SessionUtil.createUserIdAndUserSession();
 		MvcResult mvcResult = mockMvc.perform(get("/shop/show_order_history")// 元のURL
